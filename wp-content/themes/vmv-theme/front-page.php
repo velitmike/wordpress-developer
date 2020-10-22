be565f959a0b07be812079d9e4b301ba9517465c
velitmike <mvelitchenko@yandex.ru>
2020-10-22 12:26:31 +1200

    create: added  sidebar Downloader_Widget

--- wp-content/themes/vmv-theme/front-page.php
+++ wp-content/themes/vmv-theme/front-page.php
@@ -1,285 +1,255 @@
-<?php get_header();?>
+<?php get_header(); ?>
 <main class="front-page-header">
-  <div class="container">
-    <div class="hero">
-      <div class="left">
-        <?php
+	<div class="container">
+		<div class="hero">
+			<div class="left">
+				<?php
 				global $post;
 				// объявляем глобальную прерменную 
 				$myposts = get_posts([
 					'numberposts' =>
-				1, 'category_name' => 'javascript, css, html, web-design', ]);
-				 //  Проверяем, есть ли посты
-				  if( $myposts) { 
-						// если есть, запускаем цикл
-        foreach( $myposts as $post ){ setup_postdata( $post ); ?>
-        <!-- выводим записи	-->
-        <img
-          src="<?php the_post_thumbnail_url()?>"
-          alt="Фото: Компьтеры"
-          class="post-thumb"
-        />
-        <?php $author_id = get_the_author_meta('ID'); ?>
-        <a href="<?php echo get_author_posts_url($author_id)?>" class="author">
-          <img
-            src="<?php echo get_avatar_url($author_id)?>"
-            alt="Михаил Велитченко"
-            class="avatar"
-          />
-          <div class="author-bio">
-            <span class="author-name"><?php the_author(); ?></span>
-            <span class="author-rank">Должность</span>
-          </div>
-        </a>
-        <div class="post-text">
-          <?php the_category(); ?>
-          <h2 class="post-title">
-            <?php echo mb_strimwidth(get_the_title(), 0, 60, '...'); ?>
-          </h2>
-          <a href="<?php echo get_the_permalink() ?>" class="more"
-            >Читать далее</a
-          >
-        </div>
-        <?php 	
-			}
-			} else {
+					1, 'category_name' => 'javascript, css, html, web-design',
+				]);
+				//  Проверяем, есть ли посты
+				if ($myposts) {
+					// если есть, запускаем цикл
+					foreach ($myposts as $post) {
+						setup_postdata($post); ?>
+						<!-- выводим записи	-->
+						<img src="<?php the_post_thumbnail_url() ?>" alt="Фото: Компьтеры" class="post-thumb" />
+						<?php $author_id = get_the_author_meta('ID'); ?>
+						<a href="<?php echo get_author_posts_url($author_id) ?>" class="author">
+							<img src="<?php echo get_avatar_url($author_id) ?>" alt="Михаил Велитченко" class="avatar" />
+							<div class="author-bio">
+								<span class="author-name"><?php the_author(); ?></span>
+								<span class="author-rank">Должность</span>
+							</div>
+						</a>
+						<div class="post-text">
+							<?php the_category(); ?>
+							<h2 class="post-title">
+								<?php echo mb_strimwidth(get_the_title(), 0, 60, '...'); ?>
+							</h2>
+							<a href="<?php echo get_the_permalink() ?>" class="more">Читать далее</a>
+						</div>
+					<?php
+					}
+				} else {
+					?>
+					<p>Постов нет</p>
+				<?php
+				}
+				wp_reset_postdata();
 				?>
-        <p>Постов нет</p>
-        <?php
-			}
-			wp_reset_postdata();
-			?>
-      </div>
-      <!-- /.left -->
-      <div class="right">
-        <h3 class="recommend">Рекомендуем</h3>
-        <ul class="posts-list">
-          <?php
+			</div>
+			<!-- /.left -->
+			<div class="right">
+				<h3 class="recommend">Рекомендуем</h3>
+				<ul class="posts-list">
+					<?php
 					global $post;
 					// объявляем глобальную прерменную 
 					$myposts = get_posts([
 						'numberposts' =>
-          5, 'offset' => 1, 'category_name' => 'javascript, css, html,
-					web-design', ]); 
+						5, 'offset' => 1, 'category_name' => 'javascript, css, html,
+					web-design',
+					]);
 					// Проверяем, есть ли посты? 
-					if( $myposts) { 
+					if ($myposts) {
 						// если есть, запускаем цикл 
-						foreach( $myposts as $post ){ setup_postdata(
-          $post ); ?>
-          <li class="post">
-            <?php the_category(); ?>
-            <a class="post-permalink" href="<?php echo get_the_permalink( )?>">
-              <h4 class="post-title">
-                <?php echo mb_strimwidth(get_the_title(), 0, 60, '...'); ?>
-              </h4>
-            </a>
-          </li>
-          <?php 	
+						foreach ($myposts as $post) {
+							setup_postdata(
+								$post
+							); ?>
+							<li class="post">
+								<?php the_category(); ?>
+								<a class="post-permalink" href="<?php echo get_the_permalink() ?>">
+									<h4 class="post-title">
+										<?php echo mb_strimwidth(get_the_title(), 0, 60, '...'); ?>
+									</h4>
+								</a>
+							</li>
+						<?php
 						}
-						} else {
-							?>
-          <p>Постов нет</p>
-          <?php
-						}
-						wp_reset_postdata();
+					} else {
 						?>
-        </ul>
-      </div>
-      <!-- /.right -->
-    </div>
-    <!-- /.hero -->
-  </div>
-  <!-- /.container -->
+						<p>Постов нет</p>
+					<?php
+					}
+					wp_reset_postdata();
+					?>
+				</ul>
+			</div>
+			<!-- /.right -->
+		</div>
+		<!-- /.hero -->
+	</div>
+	<!-- /.container -->
 </main>
 <div class="container">
-  <ul class="article-list">
-    <?php
-						global $post;
-						// объявляем глобальную прерменную 
-						$myposts = get_posts([
-							'numberposts' => 4,
-							'category_name' => 'articles', ]); 
+	<ul class="article-list">
+		<?php
+		global $post;
+		// объявляем глобальную прерменную 
+		$myposts = get_posts([
+			'numberposts' => 4,
+			'category_name' => 'articles',
+		]);
 		// Проверяем, есть ли посты?
-		 if($myposts) { 
-			 // если есть, запускаем цикл 
-			 foreach( $myposts as $post ){
-    setup_postdata( $post ); ?>
-    <li class="article-item">
-      <a class="article-permalink" href="<?php echo get_the_permalink( )?>">
-        <h4 class="article-title">
-          <?php echo mb_strimwidth(get_the_title(), 0, 50, '...'); ?>
-        </h4>
-      </a>
-      <img
-        width="65"
-        height="65"
-        src="<?php the_post_thumbnail_url(null, 'thumbnail')?>"
-        alt="Миниатюры"
-      />
-    </li>
-    <?php 	
-			}
-			} else {
-				?>
-    <p>Постов нет</p>
-    <?php
-			}
-			wp_reset_postdata();
-			?></ul>
-  <!--/.article-list  -->
-  <ul class="article-grid">
-    <?php		
-			global $post;
-			// формируем запрос в базу данных
-
-			$query = new WP_Query( [
-				// Получаем 7 постов
-				'posts_per_page' =>
-		7, ] ); 
-		// провераем, если посты 
-		if ( $query->have_posts() ) { 
-			// создаем  переменнрую-счетчик постов 
-			$cnt = 0; /* cnt - counter */ 
-			// пока посты есть,	выводим их 
-		while ( $query->have_posts() ) { $query->the_post(); 
-			// увеличиваем счетчик постов 
-			$cnt++; 
-			switch ($cnt) {
-				 // Выводим первый пост
-    case '1': ?>
-    <li class="article-grid-item article-grid-item-1">
-      <a href="<?php the_permalink() ?>" class="article-grid-permalink">
-        <span class="category-name"
-          ><?php $category = get_the_category(); 
-			echo $category[0]->name;; ?></span
-        >
-        <h4 class="article-grid-title">
-          <?php echo mb_strimwidth(get_the_title(), 0, 60, '...');?>
-        </h4>
-        <p class="article-grid-excerpt">
-          <?php echo mb_strimwidth(get_the_excerpt(), 0, 200, '...'); ?>
-        </p>
-        <div class="article-grid-info">
-          <div class="author">
-            <?php $author_id = get_the_author_meta('ID'); ?>
-            <img
-              src="<?php echo get_avatar_url($author_id)?>"
-              alt="Михаил Велитченко"
-              class="author-avatar"
-            />
-            <span class="author-name"
-              ><strong><?php the_author()?></strong> :
-              <?php echo mb_strimwidth(get_the_author_meta('description'), 0, 26, '...');?>
-            </span>
-          </div>
-          <div class="comments">
-            <img
-              src="<?php echo get_template_directory_uri(  ) . '/assets/images/comment.svg'?>"
-              alt="Icon: Комментарии"
-              class="comments-icon"
-            />
-            <span class="comments-counter"
-              ><?php comments_number('0', '1', '%')?></span
-            >
-          </div>
-        </div>
-      </a>
-    </li>
-    <!-- /.article-grid-item -->
-    <?php
-							break;
-							// Выводим второй пост
-							case '2': ?>
-    <li class="article-grid-item article-grid-item-2">
-			<img src="<?php echo get_the_post_thumbnail_url()?>" alt="" class="article-grid-thrumb">
-			<a href="<?php the_permalink() ?>" class="article-grid-permalink">
-			<span class="tag">
-				<?php $posttags = get_the_tags();
-					if ( $posttags ) {
-						echo $posttags[0]->name . ' ';
-					}
-					?>
-			</span>
-        <span class="category-name"
-          ><?php $category = get_the_category(); echo $category[0]->name;; ?></span>
-        <h4 class="article-grid-title">
-          <?php the_title()?>
-        </h4>
-        <div class="article-grid-info">
-          <div class="author">
-            <?php $author_id = get_the_author_meta('ID'); ?>
-              <img
-              src="<?php echo get_avatar_url($author_id)?>"
-              alt="Михаил Велитченко"
-              class="author-avatar"/>	
-								
-							<div class="author-info">
-								<span class="author-name"><strong><?php the_author()?></strong></span>
-								<span class="date"><?php the_time('j F')?></span>
-								<div class="comments">
-									<img
-										src="<?php echo get_template_directory_uri(  ) . '/assets/images/comment-white.svg'?>"
-										alt="Icon: Комментарии"
-										class="comments-icon"
-									/>
-									<span class="comments-counter"
-										><?php comments_number('0', '1', '%')?></span
-									>						
-								</div>
-								<div class="likes">
-									<img src="<?php echo get_template_directory_uri(  ) . '/assets/images/heart.svg'?>" alt="icon: like" class="likes-icon">
-										<span class="likes-counter"
-										><?php comments_number('0', '1', '%')?></span
-										>						
-								</div>
-							</div>
-							<!-- /.author-info -->    
-					</div>
-        </div>
-      </a>
-    </li>
-		<?php	
-		break;
-
-			// Выводим третий пост
-			case '3': ?>
-			<li class="article-grid-item article-grid-item-3">
-				<a href="<?php the_permalink() ?>" class="article-grid-permalink">
-				<img src="<?php echo get_the_post_thumbnail_url()?>" alt="Миниатюры" class="article-grid-thumb">
-				<h4 class="article-grid-title">
-				<?php echo the_title()?>
-				</h4>
-				</a>
-			</li>
-			<?php	
-			break;
-
-			// выводим остальные посты
-				default: ?>
-				<li class="article-grid-item article-grid-item-default">
-				<a href="<?php the_permalink() ?>" class="article-grid-permalink">
-						<h4 class="article-grid-title">
-						<?php echo mb_strimwidth(get_the_title(), 0, 33, '...');?>
+		if ($myposts) {
+			// если есть, запускаем цикл 
+			foreach ($myposts as $post) {
+				setup_postdata($post); ?>
+				<li class="article-item">
+					<a class="article-permalink" href="<?php echo get_the_permalink() ?>">
+						<h4 class="article-title">
+							<?php echo mb_strimwidth(get_the_title(), 0, 50, '...'); ?>
 						</h4>
-						<p class="article-grid-excerpt">
-            <?php echo mb_strimwidth(get_the_excerpt(), 0, 76, '...') ?>
-						</p>
-						<span class="article-date"><?php the_time('j F Y')?></span>
-				</a>
+					</a>
+					<img width="65" height="65" src="<?php the_post_thumbnail_url(null, 'thumbnail') ?>" alt="Миниатюры" />
 				</li>
+			<?php
+			}
+		} else {
+			?>
+			<p>Постов нет</p>
+		<?php
+		}
+		wp_reset_postdata();
+		?></ul>
+	<!--/.article-list  -->
+	<ul class="article-grid">
+		<?php
+		global $post;
+		// формируем запрос в базу данных
+
+		$query = new WP_Query([
+			// Получаем 7 постов
+			'posts_per_page' =>
+			7,
+		]);
+		// провераем, если посты 
+		if ($query->have_posts()) {
+			// создаем  переменнрую-счетчик постов 
+			$cnt = 0; /* cnt - counter */
+			// пока посты есть,	выводим их 
+			while ($query->have_posts()) {
+				$query->the_post();
+				// увеличиваем счетчик постов 
+				$cnt++;
+				switch ($cnt) {
+						// Выводим первый пост
+					case '1': ?>
+						<li class="article-grid-item article-grid-item-1">
+							<a href="<?php the_permalink() ?>" class="article-grid-permalink">
+								<span class="category-name"><?php $category = get_the_category();
+																						echo $category[0]->name;; ?></span>
+								<h4 class="article-grid-title">
+									<?php echo mb_strimwidth(get_the_title(), 0, 60, '...'); ?>
+								</h4>
+								<p class="article-grid-excerpt">
+									<?php echo mb_strimwidth(get_the_excerpt(), 0, 200, '...'); ?>
+								</p>
+								<div class="article-grid-info">
+									<div class="author">
+										<?php $author_id = get_the_author_meta('ID'); ?>
+										<img src="<?php echo get_avatar_url($author_id) ?>" alt="Михаил Велитченко" class="author-avatar" />
+										<span class="author-name"><strong><?php the_author() ?></strong> :
+											<?php echo mb_strimwidth(get_the_author_meta('description'), 0, 26, '...'); ?>
+										</span>
+									</div>
+									<div class="comments">
+										<img src="<?php echo get_template_directory_uri() . '/assets/images/comment.svg' ?>" alt="Icon: Комментарии" class="comments-icon" />
+										<span class="comments-counter"><?php comments_number('0', '1', '%') ?></span>
+									</div>
+								</div>
+							</a>
+						</li>
+						<!-- /.article-grid-item -->
+					<?php
+						break;
+						// Выводим второй пост
+					case '2': ?>
+						<li class="article-grid-item article-grid-item-2">
+							<img src="<?php echo get_the_post_thumbnail_url() ?>" alt="Миниатюры" class="article-grid-thrumb">
+							<a href="<?php the_permalink() ?>" class="article-grid-permalink">
+								<span class="tag">
+									<?php $posttags = get_the_tags();
+									if ($posttags) {
+										echo $posttags[0]->name . ' ';
+									}
+									?>
+								</span>
+								<span class="category-name"><?php $category = get_the_category();
+								echo $category[0]->name;; ?></span>
+								<h4 class="article-grid-title">
+									<?php the_title() ?>
+								</h4>
+								<div class="article-grid-info">
+									<div class="author">
+										<?php $author_id = get_the_author_meta('ID'); ?>
+										<img src="<?php echo get_avatar_url($author_id) ?>" alt="Михаил Велитченко" class="author-avatar" />
+
+										<div class="author-info">
+											<span class="author-name"><strong><?php the_author() ?></strong></span>
+											<span class="date"><?php the_time('j F') ?></span>
+											<div class="comments">
+												<img src="<?php echo get_template_directory_uri() . '/assets/images/comment-white.svg' ?>" alt="Icon: Комментарии" class="comments-icon" />
+												<span class="comments-counter"><?php comments_number('0', '1', '%') ?></span>
+											</div>
+											<div class="likes">
+												<img src="<?php echo get_template_directory_uri() . '/assets/images/heart.svg' ?>" alt="icon: like" class="likes-icon">
+												<span class="likes-counter"><?php comments_number('0', '1', '%') ?></span>
+											</div>
+										</div>
+										<!-- /.author-info -->
+									</div>
+								</div>
+							</a>
+						</li>
+					<?php
+						break;
+
+						// Выводим третий пост
+					case '3': ?>
+						<li class="article-grid-item article-grid-item-3">
+							<a href="<?php the_permalink() ?>" class="article-grid-permalink">
+								<img src="<?php echo get_the_post_thumbnail_url() ?>" alt="Миниатюры" class="article-grid-thumb">
+								<h4 class="article-grid-title">
+									<?php echo the_title() ?>
+								</h4>
+							</a>
+						</li>
+					<?php
+						break;
+
+						// выводим остальные посты
+					default: ?>
+						<li class="article-grid-item article-grid-item-default">
+							<a href="<?php the_permalink() ?>" class="article-grid-permalink">
+								<h4 class="article-grid-title">
+									<?php echo mb_strimwidth(get_the_title(), 0, 33, '...'); ?>
+								</h4>
+								<p class="article-grid-excerpt">
+									<?php echo mb_strimwidth(get_the_excerpt(), 0, 76, '...') ?>
+								</p>
+								<span class="article-date"><?php the_time('j F Y') ?></span>
+							</a>
+						</li>
 				<?php
-				break;
+						break;
 				}
 				?>
-    <!-- Вывода постов, функции цикла: the_title() и т.д. -->
-    <?php 
-				}
-			} else {
-				// Постов не найдено
+				<!-- Вывода постов, функции цикла: the_title() и т.д. -->
+		<?php
 			}
+		} else {
+			// Постов не найдено
+		}
 
-			wp_reset_postdata(); // Сбрасываем $post
-			?></ul>
-  <!-- /.article-grid -->
+		wp_reset_postdata(); // Сбрасываем $post
+		?></ul>
+	<!-- /.article-grid -->
+	<?php get_sidebar()?>
 </div>
 <!-- /.container -->
--- wp-content/themes/vmv-theme/functions.php
+++ wp-content/themes/vmv-theme/functions.php
@@ -1,44 +1,202 @@
 <?php
 // Добавление расширенных воможностей
-if ( ! function_exists( 'vmv_theme_setup' ) ) :
-	
-function vmv_theme_setup() {
-	// Добавление тега title
-	add_theme_support( 'title-tag' );
-	// Добавления миниатюр
-	add_theme_support( 'post-thumbnails', array( 'post' ) );
-	// Добавление пользоватьского логотипа
-	add_theme_support( 'custom-logo', [
+if (!function_exists('vmv_theme_setup')) :
+
+	function vmv_theme_setup()
+	{
+		// Добавление тега title
+		add_theme_support('title-tag');
+		// Добавления миниатюр
+		add_theme_support('post-thumbnails', array('post'));
+		// Добавление пользоватьского логотипа
+		add_theme_support('custom-logo', [
 			'width'       => 163,
 			'flex-height' => true,
 			'header-text' => 'VMV_developer',
 			'unlink-homepage-logo' => false, // WP 5.5
-		] ); 
+		]);
 		// Регистрация меню
-		register_nav_menus( [
+		register_nav_menus([
 			'header_menu' => 'Menu in header',
 			'footer_menu' => 'Menu in footer'
-		] );
-}
+		]);
+	}
 endif;
-add_action( 'after_setup_theme', 'vmv_theme_setup' );
+add_action('after_setup_theme', 'vmv_theme_setup');
+
+/**
+ * Подключения сайдбара (widget area).
+ *
+ * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+ */
+function vmv_theme_widgets_init()
+{
+	register_sidebar(
+		array(
+			'name'          => esc_html__('Sidebar in main', 'vmv-theme'),
+			'id'            => 'main-sidebar',
+			'description'   => esc_html__('Add widgets here.', 'vmv-theme'),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
+}
+add_action('widgets_init', 'vmv_theme_widgets_init');
+
+
+/**
+ * Добавление нового виджета Downloader_Widget.
+ */
+class Downloader_Widget extends WP_Widget
+{
+
+	// Регистрация виджета используя основной класс
+	function __construct()
+	{
+		// вызов конструктора выглядит так:
+		// __construct( $id_base, $name, $widget_options = array(), $control_options = array() )
+		parent::__construct(
+			'downloader_widget', // ID виджета, если не указать (оставить ''), то ID будет равен названию класса в нижнем регистре: downloader_widget
+			'Полезные файлы',
+			array('description' => 'Файлы для скачивания', 'classname' => 'downloader_widget',)
+		);
+
+		// скрипты/стили виджета, только если он активен
+		if (is_active_widget(false, false, $this->id_base) || is_customize_preview()) {
+			add_action('wp_enqueue_scripts', array($this, 'add_downloader_widget_scripts'));
+			add_action('wp_head', array($this, 'add_downloader_widget_style'));
+		}
+	}
+
+	/**
+	 * Вывод виджета во Фронт-энде
+	 *
+	 * @param array $args     аргументы виджета.
+	 * @param array $instance сохраненные данные из настроек
+	 */
+	function widget($args, $instance)
+	{
+		// $title = apply_filters('widget_title', $instance['title']);
+		$title = $instance['title'];
+		$description = $instance['description'];
+		$link = $instance['link'];
+
+		echo $args['before_widget'];
+		if (!empty($title)) {
+			echo $args['before_title'] . $title . $args['after_title'];
+		}
+		if (!empty($description)) {
+			echo '<p>' . $description . '</p>';
+		}
+		if (!empty($link)) {
+			echo '<a href="' . $link . '">Скачать</a>';
+		}
+		// echo __('Hello, World!', 'text_domain');
+		echo $args['after_widget'];
+	}
+
+	/**
+	 * Админ-часть виджета
+	 *
+	 * @param array $instance сохраненные данные из настроек
+	 */
+	function form($instance)
+	{
+		$title = @$instance['title'] ?: 'Полезные файлы';
+		$description = @$instance['description'] ?: 'Описание';
+		$link = @$instance['link'] ?: 'http://yandex.ru';
+?>
+		<p>
+			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>">
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('description'); ?>"><?php _e('Описание:'); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id('description'); ?>" name="<?php echo $this->get_field_name('description'); ?>" type="text" value="<?php echo esc_attr($description); ?>">
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('link'); ?>"><?php _e('Ссылка на файл:'); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id('link'); ?>" name="<?php echo $this->get_field_name('link'); ?>" type="text" value="<?php echo esc_attr($link); ?>">
+		</p>
+	<?php
+	}
+
+	/**
+	 * Сохранение настроек виджета. Здесь данные должны быть очищены и возвращены для сохранения их в базу данных.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance новые настройки
+	 * @param array $old_instance предыдущие настройки
+	 *
+	 * @return array данные которые будут сохранены
+	 */
+	function update($new_instance, $old_instance)
+	{
+		$instance = array();
+		$instance['title'] = (!empty($new_instance['title'])) ? strip_tags($new_instance['title']) : '';
+		$instance['description'] = (!empty($new_instance['description'])) ? strip_tags($new_instance['description']) : '';
+		$instance['link'] = (!empty($new_instance['link'])) ? strip_tags($new_instance['link']) : '';
+
+		return $instance;
+	}
+
+	// скрипт виджета
+	function add_my_widget_scripts()
+	{
+		// фильтр чтобы можно было отключить скрипты
+		if (!apply_filters('show_my_widget_script', true, $this->id_base))
+			return;
+
+		$theme_url = get_stylesheet_directory_uri();
+
+		wp_enqueue_script('my_widget_script', $theme_url . '/my_widget_script.js');
+	}
+
+	// стили виджета
+	function add_my_widget_style()
+	{
+		// фильтр чтобы можно было отключить стили
+		if (!apply_filters('show_my_widget_style', true, $this->id_base))
+			return;
+	?>
+		<style type="text/css">
+			.my_widget a {
+				display: inline;
+			}
+		</style>
+<?php
+	}
+}
+// конец класса Downloader_Widget
+
+// регистрация Downloader_Widget в WordPress
+function register_downloader_widget()
+{
+	register_widget('Downloader_Widget');
+}
+add_action('widgets_init', 'register_downloader_widget');
 
 // Подключение стилей и скриптов; при time() - файл стилей не попадает в кэш на время разработки 
-function enqueue_vmv_style() {
-	wp_enqueue_style( 'style', get_stylesheet_uri() );
-	wp_enqueue_style( 'vmv-theme', get_template_directory_uri( ) . '/assets/css/vmv-theme.css', 'style', time());
-	wp_enqueue_style( 'Roboto-Slab', "https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@700&display=swap",);
+function enqueue_vmv_style()
+{
+	wp_enqueue_style('style', get_stylesheet_uri());
+	wp_enqueue_style('vmv-theme', get_template_directory_uri() . '/assets/css/vmv-theme.css', 'style', time());
+	wp_enqueue_style('Roboto-Slab', "https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@700&display=swap",);
 }
-add_action( 'wp_enqueue_scripts', 'enqueue_vmv_style' );
+add_action('wp_enqueue_scripts', 'enqueue_vmv_style');
 
 ## отключаем создание миниатюр файлов для указанных размеров
-add_filter( 'intermediate_image_sizes', 'delete_intermediate_image_sizes' );
-function delete_intermediate_image_sizes( $sizes ){
+add_filter('intermediate_image_sizes', 'delete_intermediate_image_sizes');
+function delete_intermediate_image_sizes($sizes)
+{
 	// размеры которые нужно удалить
-	return array_diff( $sizes, [
+	return array_diff($sizes, [
 		'medium_large',
 		'large',
 		'1536x1536',
 		'2048x2048',
-	] );
+	]);
 }
